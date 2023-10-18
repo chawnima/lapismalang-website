e41ab2476bb8e7fced784890f5686862693faae3
chawnima <147719517+chawnima@users.noreply.github.com>
2023-10-18 19:52:45 +0700

    Add files via upload

--- slide.js
+++ slide.js
@@ -25,12 +25,3 @@ setInterval(() => changeSlide(1), 3000);
 
 showSlide(0);
 
-function navigate() {
-  var menuBox = document.getElementsByClassName('navigate-sub');    
-  if(menuBox.style.display == "block") { // if is menuBox displayed, hide it
-    menuBox.style.display = "none";
-  }
-  else { // if is menuBox hidden, display it
-    menuBox.style.display = "block";
-  }
-}
